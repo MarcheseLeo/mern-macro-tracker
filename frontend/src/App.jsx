cade8fa3e3d2403a81d1f976bb1fe9d852b1bb18
MarcheseLeo <leonardo.lol.ldp@gmail.com>
2026-07-03 20:39:47 +0200

    refactoring

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home/Home"
-import VerifyEmail from "./pages/VerifyEmail/VerifyEmail"
 import { Welcome } from './pages/Welcome/Welcome';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register'
